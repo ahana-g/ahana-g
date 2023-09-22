02713e4c7ed6ca07e1a69d2b3a478c06e2476e20
Ahana G <ahana@Ahanas-MacBook-Air.local>
2023-09-22 14:11:30 -0400

    my first shell script

---
+++ shellscript.sh
@@ -0,0 +1,9 @@
+echo Hello World
+echo What is your name
+read name
+say -v Veena $name
+echo What is your favorite ice cream flavor
+read icecream
+say -v Veena $icecream
+echo welcome $name I love $icecream too
+say -v Veena Welcome $name I love $icecream too
